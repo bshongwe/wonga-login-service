361c23069dbf7f66dee64419095b445df3b526cc
bshongwe <dru.da.beat.hustle@gmail.com>
2026-03-04 15:36:31 +0200

    feat(init): add Login, Home, Register, and User Details pages

---
+++ wonga-login-service-client/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Welcome to Wonga Login Service</h1>
+    </main>
+  );
+}
---
+++ wonga-login-service-client/app/user-details/page.tsx
@@ -0,0 +1,7 @@
+export default function UserDetailsPage() {
+  return (
+    <div>
+      <h1>Wonga User Details</h1>
+    </div>
+  );
+}
