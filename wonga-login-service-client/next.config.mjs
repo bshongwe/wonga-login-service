4f97a05cb2d8837d9482af504d706be8fba3d7c8
bshongwe <dru.da.beat.hustle@gmail.com>
2026-03-06 05:57:52 +0200

    feat(Navbar): add Wonga logo to the navigation bar feat(next.config): configure remote image patterns for Wonga logo

--- wonga-login-service-client/next.config.mjs
+++ wonga-login-service-client/next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.liblogo.com',
+        pathname: '/img-logo/**',
+      },
+    ],
+  },
   eslint: {
     // Run ESLint on these directories during builds
     dirs: ['pages', 'components', 'lib', 'app', 'src'],
